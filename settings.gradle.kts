6a55ed1a1b095d4b5bf0b3410052343824214810
Matúš Sulír <matus.sulir@gmail.com>
2025-03-27 17:57:30 +0100

    Speed up Gradle by caching

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,13 @@ plugins {
     id("de.fayard.refreshVersions") version "0.60.5"
 }
 
+refreshVersions {
+    rejectVersionIf {
+        @Suppress("UnstableApiUsage")
+        candidate.stabilityLevel != de.fayard.refreshVersions.core.StabilityLevel.Stable
+    }
+}
+
 rootProject.name = "runtimesave"
 include("shared", "starter", "plugin")
 project(":shared").name = "runtimesave-shared"
