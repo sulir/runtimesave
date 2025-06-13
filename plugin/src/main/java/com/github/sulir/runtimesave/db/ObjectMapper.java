f653abcd18296f85f80f4a75bb7913b1c76af64a
Matúš Sulír <matus.sulir@gmail.com>
2025-06-13 23:12:27 +0200

    #23 Use MethodHandles for speed and add exception un-checker

--- plugin/src/main/java/com/github/sulir/runtimesave/db/ObjectMapper.java
+++ plugin/src/main/java/com/github/sulir/runtimesave/db/ObjectMapper.java
@@ -5,6 +5,8 @@ import org.neo4j.driver.Value;
 import org.neo4j.driver.types.Node;
 import org.neo4j.driver.types.Relationship;
 
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
@@ -13,6 +15,7 @@ import java.util.*;
 public class ObjectMapper {
     private static final Map<Class<? extends GraphNode>, ObjectMapper> classToMapper = new HashMap<>();
     private static final Map<String, ObjectMapper> stringToMapper = new HashMap<>();
+    private static final MethodHandles.Lookup lookup = MethodHandles.publicLookup();
 
     private final Class<? extends GraphNode> clazz;
     private final String label;
@@ -25,16 +28,20 @@ public class ObjectMapper {
     }
 
     public static ObjectMapper getInstance(String nodeLabel) {
-        return stringToMapper.computeIfAbsent(nodeLabel, label -> getInstance(findClass(nodeLabel)));
+        return stringToMapper.computeIfAbsent(nodeLabel, label -> getInstance(uncheck(() -> findClass(nodeLabel))));
     }
 
-    private static Class<? extends GraphNode> findClass(String nodeLabel) {
+    public ObjectMapper(Class<? extends GraphNode> clazz) {
+        this.clazz = clazz;
+        label = clazz.getSimpleName().substring(0, clazz.getSimpleName().lastIndexOf("Node"));
+        constructor = findConstructor();
+        findProperties();
+        findRelations();
+    }
+
+    private static Class<? extends GraphNode> findClass(String nodeLabel) throws ClassNotFoundException {
         String packageName = GraphNode.class.getPackageName();
-        try {
-            return Class.forName(packageName + "." + nodeLabel + "Node").asSubclass(GraphNode.class);
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
+        return Class.forName(packageName + "." + nodeLabel + "Node").asSubclass(GraphNode.class);
     }
 
     public String getLabel() {
@@ -42,26 +49,18 @@ public class ObjectMapper {
     }
 
     public GraphNode createNode(Node node) {
-        try {
-            Object[] params = new Object[properties.size()];
-            for (int i = 0; i < params.length; i++)
-                params[i] = node.get(properties.get(i).name()).asObject();
-            return constructor.newInstance(params);
-        } catch (ReflectiveOperationException e) {
-            throw new RuntimeException(e);
-        }
+        Object[] params = new Object[properties.size()];
+        for (int i = 0; i < params.length; i++)
+            params[i] = node.get(properties.get(i).name()).asObject();
+        return uncheck(() -> constructor.newInstance(params));
     }
 
     public Map<String, Object> createProperties(GraphNode node) {
         Map<String, Object> properties = new HashMap<>();
         properties.put("id", UUID.randomUUID().toString());
 
-        try {
-            for (Property property : this.properties)
-                properties.put(property.name(), property.getter().invoke(node));
-        } catch (ReflectiveOperationException e) {
-            throw new RuntimeException(e);
-        }
+        for (Property property : this.properties)
+            uncheck(() -> properties.put(property.name(), property.getter().invoke(node)));
 
         return properties;
     }
@@ -69,15 +68,7 @@ public class ObjectMapper {
     public void createEdge(GraphNode from, GraphNode to, Relationship edge) {
         Relation relation = relations.get(edge.type());
         Value keyOrIndex = edge.get(relation.property());
-
-        try {
-            if (relation.kind() == RelationKind.MAP)
-                relation.setter().invoke(from, keyOrIndex.asString(), to);
-            else
-                relation.setter().invoke(from, keyOrIndex.asInt(), to);
-        } catch (ReflectiveOperationException e) {
-            throw new RuntimeException(e);
-        }
+        uncheck(() -> relation.setter().invoke(from, keyOrIndex.asObject(), to));
     }
 
     public List<Map<String, Object>> createOutEdges(GraphNode node, Map<GraphNode, String> nodeToId) {
@@ -85,70 +76,53 @@ public class ObjectMapper {
         String from = nodeToId.get(node);
 
         relations.forEach((type, relation) -> {
-            try {
-                Object items = relation.getter().invoke(node);
-                if (items instanceof Map<?,?> map) {
-                    map.forEach((key, value) -> edges.add(Map.of("from", from, "type", type,
-                            "props", Map.of(relation.property(), key), "to", nodeToId.get((GraphNode) value))));
-                } else if (items instanceof List<?> list) {
-                    int index = 0;
-                    for (var element : list)
-                        edges.add(Map.of("from", from, "type", type, "props", Map.of(relation.property(), index++),
-                                "to", nodeToId.get((GraphNode) element)));
-                }
-            } catch (ReflectiveOperationException e) {
-                throw new RuntimeException(e);
+            Object items = uncheck(() -> relation.getter().invoke(node));
+            if (items instanceof Map<?,?> map) {
+                map.forEach((key, value) -> edges.add(Map.of("from", from, "type", type,
+                        "props", Map.of(relation.property(), key), "to", nodeToId.get((GraphNode) value))));
+            } else if (items instanceof List<?> list) {
+                int index = 0;
+                for (var element : list)
+                    edges.add(Map.of("from", from, "type", type, "props", Map.of(relation.property(), index++),
+                            "to", nodeToId.get((GraphNode) element)));
             }
         });
 
         return edges;
     }
 
-    private ObjectMapper(Class<? extends GraphNode> clazz) {
-        this.clazz = clazz;
-        label = clazz.getSimpleName().substring(0, clazz.getSimpleName().lastIndexOf("Node"));
-        try {
-            constructor = findConstructor();
-            findProperties();
-            findRelations();
-        } catch (ReflectiveOperationException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private Constructor<? extends GraphNode> findConstructor() throws ReflectiveOperationException {
+    private Constructor<? extends GraphNode> findConstructor() {
         Constructor<?>[] constructors = clazz.getConstructors();
         if (constructors.length != 1)
-            throw new ReflectiveOperationException("Node classes must have one constructor");
-        return clazz.getConstructor(constructors[0].getParameterTypes());
+            throw new IllegalArgumentException("Node classes must have one constructor");
+        return uncheck(() -> clazz.getConstructor(constructors[0].getParameterTypes()));
     }
 
-    private void findProperties() throws NoSuchMethodException {
+    private void findProperties() {
         for (Parameter param : constructor.getParameters()) {
-            String name = param.getName();
-            Method getter = clazz.getMethod("get" + name.substring(0, 1).toUpperCase() + name.substring(1));
-            properties.add(new Property(name, getter));
+            uncheck(() -> {
+                String name = param.getName();
+                Method getter = clazz.getMethod("get" + name.substring(0, 1).toUpperCase() + name.substring(1));
+                return properties.add(new Property(name, lookup.unreflect(getter)));
+            });
         }
     }
 
-    private void findRelations() throws NoSuchMethodException {
-        for (Method setter : clazz.getMethods()) {
-            if (!setter.getName().startsWith("set"))
+    private void findRelations() {
+        for (Method method : clazz.getMethods()) {
+            if (!method.getName().startsWith("set"))
                 continue;
 
-            Parameter[] params = setter.getParameters();
+            Parameter[] params = method.getParameters();
             if (params.length != 2 || !GraphNode.class.isAssignableFrom(params[1].getType()))
                 continue;
 
-            RelationKind kind = RelationKind.fromSetterParameter(params[0].getType());
-            if (kind == null)
-                continue;
-
-            String noun = setter.getName().substring(3);
+            String noun = method.getName().substring(3);
             String type = "HAS_" + noun.toUpperCase();
             String property = params[0].getName();
-            Method getter = clazz.getMethod("get" + pluralize(noun));
-            relations.put(type, new Relation(kind, property, getter, setter));
+            MethodHandle getter = uncheck(() -> lookup.unreflect(clazz.getMethod("get" + pluralize(noun))));
+            MethodHandle setter = uncheck(() -> lookup.unreflect(method));
+            relations.put(type, new Relation(property, getter, setter));
         }
     }
 
@@ -159,15 +133,19 @@ public class ObjectMapper {
                noun + "s";
     }
 
-    private record Property(String name, Method getter) { }
+    private record Property(String name, MethodHandle getter) { }
 
-    private record Relation(RelationKind kind, String property, Method getter, Method setter) { }
+    private record Relation(String property, MethodHandle getter, MethodHandle setter) { }
 
-    private enum RelationKind {
-        MAP, LIST;
+    private interface Throwing<T> {
+        T call() throws Throwable;
+    }
 
-        public static RelationKind fromSetterParameter(Class<?> firstParamType) {
-            return Map.of(String.class, RelationKind.MAP, int.class, RelationKind.LIST).get(firstParamType);
+    private static <T> T uncheck(Throwing<T> callable) {
+        try {
+            return callable.call();
+        } catch (Throwable e) {
+            throw new RuntimeException(e);
         }
     }
 }
