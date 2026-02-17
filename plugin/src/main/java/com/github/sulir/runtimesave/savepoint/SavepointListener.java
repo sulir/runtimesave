98670466dc97c08032529e91641c37667b626536
Matúš Sulír <matus.sulir@gmail.com>
2026-02-17 21:04:19 +0100

    #2 Hide and disable Debug Tool Window during successive savepoint hits

--- plugin/src/main/java/com/github/sulir/runtimesave/savepoint/SavepointListener.java
+++ plugin/src/main/java/com/github/sulir/runtimesave/savepoint/SavepointListener.java
@@ -7,17 +7,34 @@ import com.intellij.debugger.impl.DebuggerSession;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.fileEditor.FileDocumentManager;
+import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Computable;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.openapi.wm.ToolWindowId;
+import com.intellij.openapi.wm.ToolWindowManager;
 import com.intellij.psi.PsiElement;
+import com.intellij.util.Alarm;
 import com.intellij.xdebugger.XDebugSessionListener;
 import com.intellij.xdebugger.XDebuggerManager;
 import com.intellij.xdebugger.breakpoints.XBreakpointManager;
 import com.sun.jdi.StackFrame;
 
+import java.util.function.Consumer;
+
 import static com.github.sulir.runtimesave.UncheckedThrowing.uncheck;
 
 public class SavepointListener implements DebuggerManagerListener {
+    public static final int DEBUG_WINDOW_DISABLED_MS = 1000;
+
+    private final Project project;
+    private final Alarm reEnableWindowSchedule = new Alarm(Alarm.ThreadToUse.SWING_THREAD);
+    private boolean windowWasVisible = false;
+
+    public SavepointListener(Project project) {
+        this.project = project;
+    }
+
     @Override
     public void sessionAttached(DebuggerSession session) {
         if (session.getXDebugSession() == null)
@@ -27,6 +44,7 @@ public class SavepointListener implements DebuggerManagerListener {
             @Override
             public void sessionPaused() {
                 if (isPausedAtSavepoint(session)) {
+                    disableDebugWindowTemporarily();
                     StackFrameProxy proxy = session.getContextManager().getContext().getFrameProxy();
                     if (proxy == null)
                         return;
@@ -35,7 +53,16 @@ public class SavepointListener implements DebuggerManagerListener {
                     RuntimeStorageService.getInstance().saveFrame(frame);
 
                     ApplicationManager.getApplication().invokeLater(session::resume);
+                } else {
+                    if (!reEnableWindowSchedule.isEmpty())
+                        reEnableDebugWindow();
                 }
+            }
+
+            @Override
+            public void sessionStopped() {
+                if (!reEnableWindowSchedule.isEmpty())
+                    reEnableDebugWindow();
             }
         });
     }
@@ -64,5 +91,33 @@ public class SavepointListener implements DebuggerManagerListener {
 
         return manager.findBreakpointsAtLine(savepointType, file, line).stream().anyMatch(b ->
                 b.getType() instanceof SavepointType);
+    }
+
+    private void disableDebugWindowTemporarily() {
+        doWithDebugToolWindow((window) -> {
+            if (reEnableWindowSchedule.isEmpty())
+                windowWasVisible = window.isVisible();
+
+            window.setAvailable(false);
+            reEnableWindowSchedule.cancelAllRequests();
+            reEnableWindowSchedule.addRequest(this::reEnableDebugWindow, DEBUG_WINDOW_DISABLED_MS);
+        });
+    }
+
+    private void reEnableDebugWindow() {
+        doWithDebugToolWindow((window) -> {
+            window.setAvailable(true);
+            if (windowWasVisible)
+                window.show();
+        });
+    }
+
+    private void doWithDebugToolWindow(Consumer<ToolWindow> action) {
+        ToolWindowManager manager = ToolWindowManager.getInstance(project);
+        manager.invokeLater(() -> {
+            ToolWindow window = manager.getToolWindow(ToolWindowId.DEBUG);
+            if (window != null)
+                action.accept(window);
+        });
     }
 }
