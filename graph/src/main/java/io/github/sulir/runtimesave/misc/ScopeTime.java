3088e9781ec321a83781eede8f3454af2d749604
Matúš Sulír <matus.sulir@gmail.com>
2026-04-01 21:39:43 +0200

    Add scope timer for benchmarking converted to Java

---
+++ graph/src/main/java/io/github/sulir/runtimesave/misc/ScopeTime.java
@@ -0,0 +1,45 @@
+package io.github.sulir.runtimesave.misc;
+
+import java.util.concurrent.atomic.LongAdder;
+
+@SuppressWarnings("unused")
+public final class ScopeTime implements AutoCloseable {
+    private static final int MAX_TIMERS = 32;
+    private static final LongAdder[] totals = new LongAdder[MAX_TIMERS];
+
+    static {
+        for (int i = 0; i < MAX_TIMERS; i++) {
+            totals[i] = new LongAdder();
+        }
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            for (int i = 0; i < MAX_TIMERS; i++) {
+                long nanos = totals[i].sum();
+                if (nanos != 0) {
+                    System.err.printf("Java timer %d: %d ms%n", i, nanos / 1_000_000);
+                }
+            }
+        }));
+    }
+
+    private final int id;
+    private final long startNanos;
+
+    public ScopeTime() {
+        this(0);
+    }
+
+    public ScopeTime(int id) {
+        if (id < 0 || id >= MAX_TIMERS) {
+            throw new IllegalArgumentException("Timer ID out of range: " + id);
+        }
+        this.id = id;
+        this.startNanos = System.nanoTime();
+    }
+
+    @Override
+    public void close() {
+        long diff = System.nanoTime() - startNanos;
+        totals[id].add(diff);
+    }
+}
