b94cd1eea88207fcc1d7745c59875e32d5cbc2b2
Matúš Sulír <matus.sulir@gmail.com>
2026-05-02 21:01:07 +0200

    #55 Fix newly started RuntimeSave threads not being resumed

--- plugin/src/main/java/io/github/sulir/runtimesave/savepoint/SavepointListener.java
+++ plugin/src/main/java/io/github/sulir/runtimesave/savepoint/SavepointListener.java
@@ -1,14 +1,19 @@
 package io.github.sulir.runtimesave.savepoint;
 
+import com.intellij.debugger.engine.DebuggerManagerThreadImpl;
+import com.intellij.debugger.engine.events.DebuggerCommandImpl;
 import com.intellij.debugger.impl.DebuggerManagerListener;
 import com.intellij.debugger.impl.DebuggerSession;
 import com.intellij.openapi.util.UserDataHolderEx;
+import com.intellij.util.concurrency.AppExecutorUtil;
 import com.intellij.xdebugger.XDebugSession;
 import com.intellij.xdebugger.XDebugSessionListener;
 import com.sun.jdi.ThreadReference;
 import com.sun.jdi.VirtualMachine;
 import io.github.sulir.runtimesave.config.RuntimeSaveSettings;
 import io.github.sulir.runtimesave.misc.BoundedExecutor;
+
+import java.util.concurrent.TimeUnit;
 
 public class SavepointListener implements DebuggerManagerListener {
     @Override
@@ -30,6 +35,9 @@ public class SavepointListener implements DebuggerManagerListener {
     }
 
     private void resumeOurThreads(DebuggerSession session) {
+        if (!session.isPaused())
+            return;
+
         VirtualMachine vm = session.getProcess().getVirtualMachineProxy().getVirtualMachine();
         for (ThreadReference thread : vm.allThreads()) {
             if (thread.name().startsWith(BoundedExecutor.PREFIX) || thread.name().startsWith("Neo4jDriverIO")) {
@@ -38,5 +46,21 @@ public class SavepointListener implements DebuggerManagerListener {
                     thread.resume();
             }
         }
+
+        scheduleNextThreadsResume(session);
+    }
+
+    private void scheduleNextThreadsResume(DebuggerSession session) {
+        AppExecutorUtil.getAppScheduledExecutorService().schedule(() -> {
+            DebuggerManagerThreadImpl managerThread = session.getContextManager().getContext().getManagerThread();
+            if (managerThread == null)
+                return;
+            managerThread.schedule(new DebuggerCommandImpl() {
+                @Override
+                protected void action() {
+                    resumeOurThreads(session);
+                }
+            });
+        }, 500, TimeUnit.MILLISECONDS);
     }
 }
